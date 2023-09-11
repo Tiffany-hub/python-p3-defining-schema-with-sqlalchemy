b769a7437d600bb1a0002b77c14eea7f57f3eed8
+ <+>
2023-09-11 11:39:20 +0300

    completed lab

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+from sqlalchemy_sandbox import Base, Student
 
-from sqlalchemy_sandbox import Student
-
+# Create the SQLite database and tables
 engine = create_engine('sqlite:///students.db')
+Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
